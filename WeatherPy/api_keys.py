124aa75562ee7b2da6c3d98aca0ae6dba966155d
xliu77973 <xliu77973@gmail.com>
2024-10-21 22:11:10 -0500

    Module 6 updates

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "e6d67cdf627b0c936b46c779a0db590a"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "3e04342e2cc848f9bf07c5acca82ab8b"
